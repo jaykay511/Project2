e83c81f2cfd3510c874f466f2f96cc488b6a9785
vucat2000 <cnikolai@earthlink.net>
2019-05-11 14:34:54 -0400

    api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,17 +8,80 @@ module.exports = function(app) {
     });
   });
 
-  // Create a new example
+  // Create a new doctor
   app.post("/api/doctors", function(req, res) {
     db.Doctor.create(req.body).then(function(dbDoctor) {
       res.json(dbDoctor);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  // Delete a doctor by id
+  app.delete("/api/doctors/:id", function(req, res) {
     db.Doctor.destroy({ where: { id: req.params.id } }).then(function(dbDoctor) {
       res.json(dbDoctor);
+    });
+  });
+
+  // Get all patients
+  app.get("/api/patients", function(req, res) {
+    db.Patient.findAll({}).then(function(dbPatients) {
+      res.json(dbPatients);
+    });
+  });
+
+  // Create a new patient
+  app.post("/api/patients", function(req, res) {
+    db.Patient.create(req.body).then(function(dbPatients) {
+      res.json(dbPatients);
+    });
+  });
+
+  // Delete an patients by id
+  app.delete("/api/patients/:id", function(req, res) {
+    db.Patient.destroy({ where: { id: req.params.id } }).then(function(dbPatients) {
+      res.json(dbPatients);
+    });
+  });
+
+   // Get all pills
+   app.get("/api/pills", function(req, res) {
+    db.Prescription.findAll({}).then(function(dbPills) {
+      res.json(dbPills);
+    });
+  });
+
+  // Create a new pills
+  app.post("/api/pills", function(req, res) {
+    db.Prescription.create(req.body).then(function(dbPills) {
+      res.json(dbPills);
+    });
+  });
+
+  // Delete an pills by id
+  app.delete("/api/pills/:id", function(req, res) {
+    db.Prescription.destroy({ where: { id: req.params.id } }).then(function(dbPills) {
+      res.json(dbPills);
+    });
+  });
+
+   // Get all pills
+   app.get("/api/pills", function(req, res) {
+    db.Prescription.findAll({}).then(function(dbPills) {
+      res.json(dbPills);
+    });
+  });
+
+  // Create a new pills
+  app.post("/api/pills", function(req, res) {
+    db.Prescription.create(req.body).then(function(dbPills) {
+      res.json(dbPills);
+    });
+  });
+
+  // Delete an pills by id
+  app.delete("/api/pills/:id", function(req, res) {
+    db.Prescription.destroy({ where: { id: req.params.id } }).then(function(dbPills) {
+      res.json(dbPills);
     });
   });
 };
