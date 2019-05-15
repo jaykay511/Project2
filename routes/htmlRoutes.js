e11cd2963981f2b2443e0cbc0437c8fa831af097
Jenn <jenn@Jenns-MBP.lan>
2019-05-14 20:18:46 -0700

    setting up landing page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,10 @@ module.exports = function (app) {
   //   });
   // });
 
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
+  
   app.get("/login/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
